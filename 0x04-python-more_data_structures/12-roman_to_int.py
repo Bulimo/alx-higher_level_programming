16c3b4390e5cee7b2b884b29f7b1de89293c7d69
Bulimo <ubaga91@gmail.com>
2023-06-14 13:37:42 +0000

    modified 12-roman_to_int.py file comparison logic

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,18 +4,16 @@ def roman_to_int(roman_string):
         return 0
 
     rmn = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string[0] in rmn:
-        num = 0
-        prev = rmn[roman_string[0]]
-    else:
-        return 0
+    num = 0
+    prev = 0
 
     for i in roman_string:
         if i not in rmn:
             return 0
-        if rmn[i] <= prev:
-            num = num + rmn[i]
-        else:
+        if rmn[i] > prev:
             num = num + rmn[i] - (2 * prev)
+        else:
+            num = num + rmn[i]
+
         prev = rmn[i]
     return num
