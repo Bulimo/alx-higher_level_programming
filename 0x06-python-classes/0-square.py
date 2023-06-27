b4b46d48808d0ac331e86aee652359b6cc397cdc
Bulimo <ubaga91@gmail.com>
2023-06-27 14:36:19 +0000

    file 0-square.py. pycodestyle fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" 
-Module 0-sqare:
-    defines an implementation of a square class 
 """
+Module 0-sqare:
+    defines an implementation of a square class
+"""
+
+
 class Square:
     """ A class that defines a square """
     pass
