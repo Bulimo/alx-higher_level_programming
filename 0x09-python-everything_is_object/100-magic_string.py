4a9d6114ee92a751c364fa4d5e81bedb7b4f9234
Bulimo <ubaga91@gmail.com>
2023-07-05 08:48:47 +0000

    added a better way to do the implementation

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return ("BestSchool" * 1) + (", BestSchool" * (magic_string.count - 1))
+    return "BestSchool" + (", BestSchool" * (magic_string.count - 1))
+
+# def magic_string(List = []):
+#    List.append("BestSchool")
+#    return ", ".join(List)
