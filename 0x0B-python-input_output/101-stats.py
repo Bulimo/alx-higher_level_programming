97c134c66388b62e858992ad5abe1f9017226bc9
Bulimo <ubaga91@gmail.com>
2023-07-11 22:15:47 +0000

    added docstring in 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,36 +6,29 @@ Functions:
     print_metrics()
 """
 
+import sys
 
-def print_metrics(size, status_codes):
+
+size = 0
+line_count = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+try:
+    for line in sys.stdin:
+        line = line.strip()
+        if not line:
+            continue
+
+        log = line.split(" ")
+        size += int(log[-1])
+        status_codes[log[-2]] += 1
+        line_count += 1
+        if line_count % 10 == 0:
+            print("File size: {:d}".format(size))
+            for k, v in sorted(status_codes.items()):
+                print("{:s}: {:d}".format(k, v))
+
+except KeyboardInterrupt:
     print("File size: {:d}".format(size))
     for k, v in sorted(status_codes.items()):
-        if v:
-            print("{:s}: {:d}".format(k, v))
-
-
-if __name__ == "__main__":
-    """
-    reads file input and extracts line for extraction of metrics
-    """
-    import sys
-
-    size = 0
-    line_count = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
-    try:
-        for line in sys.stdin:
-            log = [str(word) for word in line.strip().split(" ")]
-            size += int(log[-1])
-
-            if log[-2] in status_codes:
-                status_codes[log[-2]] += 1
-
-            line_count += 1
-
-            if line_count % 10 == 0:
-                print_metrics(size, status_codes)
-
-    except KeyboardInterrupt:
-        print_metrics(size, status_codes)
+        print("{:s}: {:d}".format(k, v))
