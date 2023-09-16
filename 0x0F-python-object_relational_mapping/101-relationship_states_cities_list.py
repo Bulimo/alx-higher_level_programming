b7997b015f6bd9d18182e8526327a19823046769
Eric Ubaga <ubaga91@gmail.com>
2023-09-16 12:19:44 +0300

    changed the SQL querry in the file 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,13 +12,15 @@ from sqlalchemy.orm import sessionmaker, Session
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     my_session = Session()
 
-    states = my_session.query(State).join(
-        State.cities).order_by(State.id, City.id).all()
+    states = my_session.query(State).order_by(State.id).all()
+
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
+    my_session.close()
