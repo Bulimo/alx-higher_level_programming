397b3b0594789e909115ceb46e8c363a70dd4a13
Eric Ubaga <ubaga91@gmail.com>
2023-09-29 12:44:54 +0300

    changed decoding to only last output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,10 +7,10 @@ Script that fetches https://alx-intranet.hbtn.io/status
 if __name__ == '__main__':
     import urllib.request
 
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    req = ('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
-        the_page = response.read().decode('utf-8')
+        the_page = response.read()
         print('Body response:')
         print('    - type: {}'.format(type(the_page)))
         print('    - content: {}'.format(the_page))
-        print('    - utf8 content: {}'.format(the_page))
+        print('    - utf8 content: {}'.format(the_page.decode('utf-8')))
