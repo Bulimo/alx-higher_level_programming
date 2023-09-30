f16866e936f3ad7152985743fe3c76def8cd1f26
Eric Ubaga <ubaga91@gmail.com>
2023-09-30 18:44:37 +0300

    corrected advanced task 7 file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a JSON POST request to the URL and display the response body
-curl -s -X POST --json "@$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" --data "@$2" "$1"
